66fbe9f1b612bd665ba772f7643eac2a75807184
vivian-ma97 <2211031@tongji.edu.cn>
2025-07-03 09:38:22 +0800

    Delete step2_save_norm_result.m

--- step2_save_norm_result.m
+++
@@ -1,8 +0,0 @@
-source_folder = 'C:\Users\ma_97\Desktop\干湿代码按照kenny建议讨论后\all_agap_anomaly_score\Anomaly_score';
-target_folder = 'C:\Users\ma_97\Desktop\干湿代码按照kenny建议讨论后\all_agap_anomaly_score\Nomalized_Anomaly_score';
-global_min = 0.0443876;
-global_max = 0.559387;
-
-normalize_mat_files_to_newpath(source_folder,target_folder,global_min,global_max);
-
-
